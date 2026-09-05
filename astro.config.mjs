f4506c510299a4ed9249d00427a50551f7646a87
ammyskamble <ammyskamble@users.noreply.github.com>
2026-09-05 21:06:06 +0530

    final restored domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://signresize.com',
+  site: 'https://signresize.in',
   build: {
     inlineStylesheets: 'always'
   },
